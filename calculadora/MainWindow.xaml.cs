13d8c576403836df8c94f391bb4141b235a29165
abc <abc@example.com>
2020-10-28 19:25:41 +0100

    Agregar archivos de proyecto.

---
+++ calculadora/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b1Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            pantallaTextBox.Text += button.Content.ToString();
+        }
+    }
+}
